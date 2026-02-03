9a0d315014c308d08f0cf11229738b4eb5175e80
dangtmhe182287 <dangtmhe182287@fpt.edu.vn>
2026-02-03 17:03:20 +0700

    safety

--- src/app/backend/server.js
+++ src/app/backend/server.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 const app = express();
 
-
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+app.use('/api/', limiter);
 
 const authRoutes = require('./routes/auth.routes');
 const profileRoutes = require('./routes/profile.routes');
@@ -22,7 +30,6 @@ app.use('/api/preferences', preferencesRoutes);
 app.use('/api/meal-plan', mealPlanRoutes);
 app.use('/api/meal', mealRoutes);
 app.use('/api/ingredients', ingredientRoutes);
-
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
