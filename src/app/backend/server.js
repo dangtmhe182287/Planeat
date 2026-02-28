fa2414adc9bc929ac17da42f545d4a72e8e75692
dangtmhe182287 <dangtmhe182287@fpt.edu.vn>
2026-02-28 12:11:56 +0700

    resend2

--- src/app/backend/server.js
+++ src/app/backend/server.js
@@ -15,6 +15,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
+app.set('trust proxy', 1);
 app.use('/api/', limiter);
 
 const ingredientRoutes = require('./routes/ingredient.routes');
